65ff3e62f424e75dceb817c491dd06a5ac2aed34
Jordan Manus <jlmanus17@gmail.com>
2023-10-23 15:22:06 -0400

    added ability to flip card to front of card. created new variable to house original image src.

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ console.log('connected')
 
 let backOfCard = document.querySelector(".backOfCard")
 let menuOptions = document.querySelectorAll('.menuOptions')
+let backOfCardSrc = "https://images.pexels.com/photos/3418068/pexels-photo-3418068.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
 
 backOfCard.addEventListener('click', (event) => {
     console.log("clicked!")
+    backOfCard.src = "https://images.pexels.com/photos/706194/pexels-photo-706194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 }
-)  
+)
 
 
 for (let option of menuOptions) {
     option.addEventListener('click', (event) => {
-        switch(event.target.value) {    
-            case 1: console.log("case1"); 
-            break;
+        switch (event.target.value) {
+            case 1: console.log("case1");
+                break;
             case 2: console.log("case2");
-            break;
+                break;
             case 3: console.log("case3");
-            break;
+                break;
             default: console.log("default")
         }
 
