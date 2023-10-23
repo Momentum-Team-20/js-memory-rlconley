e27e1f5fead39b50e13ccd4ef402a5e663b471a3
Kelsey Barham <kelseylbarham@gmail.com>
2023-10-23 15:06:09 -0400

    added menu options and switch statement

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 console.log('connected')
 
 let backOfCard = document.querySelector(".backOfCard")
+let menuOptions = document.querySelectorAll('.menuOptions')
+
+
 backOfCard.addEventListener('click', (event) => {
     console.log("clicked!")
-})  
+}
+)  
 
+
+for (let option of menuOptions) {
+    option.addEventListener('click', (event) => {
+        switch(event.target.value) {    
+            case 1: console.log("case1"); 
+            break;
+            case 2: console.log("case2");
+            break;
+            case 3: console.log("case3");
+            break;
+            default: console.log("default")
+        }
+
+    })
+}
