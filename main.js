b580fd0b4479ccce85e0ec8815aedfd81ca46bf8
Jordan Manus <jlmanus17@gmail.com>
2023-10-23 15:37:30 -0400

    added second card. updated JS selectors to reflect new cards added. added JS code to change card's src. added css to change size of card image.

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 console.log('connected')
 
-let backOfCard = document.querySelector(".backOfCard")
+let cards = document.querySelectorAll(".card")
 let menuOptions = document.querySelectorAll('.menuOptions')
 let backOfCardSrc = "https://images.pexels.com/photos/3418068/pexels-photo-3418068.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
+for (let card of cards) {
+    card.addEventListener('click', (event) => {
+        console.log("clicked!")
+        card.src = "https://images.pexels.com/photos/706194/pexels-photo-706194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-backOfCard.addEventListener('click', (event) => {
-    console.log("clicked!")
-    backOfCard.src = "https://images.pexels.com/photos/706194/pexels-photo-706194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+
+    }
+    )
 }
-)
 
 
 for (let option of menuOptions) {
