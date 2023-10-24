dc8011dfa114e3fd3594fcd32d54f74d87bb10ac
jmatthews64 <jon.matthews64@gmail.com>
2023-10-24 10:29:55 -0400

    Added Bulma link and styling, and created functions for cardFlip and startMenu

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 console.log('connected')
 
-let cards = document.querySelectorAll(".card")
-let menuOptions = document.querySelectorAll('.menuOptions')
+let allCards = document.querySelectorAll(".card")
+let allMenuChoices = document.querySelectorAll('.menuOptions')
 let backOfCardSrc = "https://images.pexels.com/photos/3418068/pexels-photo-3418068.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
-for (let card of cards) {
-    card.addEventListener('click', (event) => {
-        console.log("clicked!")
-        card.src = "https://images.pexels.com/photos/706194/pexels-photo-706194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-
-
-
+function flipCard (cards) {
+    console.log('flipCard function is running, you better go catch it')
+    for (let card of cards) {
+        card.addEventListener('click', (event) => {
+            console.log("clicked!")
+            console.log(card.src)
+            card.src = "https://images.pexels.com/photos/706194/pexels-photo-706194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+            console.log(card.src)
+        }
+        )
     }
-    )
 }
 
-
+function startMenu(menuOptions) {
 for (let option of menuOptions) {
     option.addEventListener('click', (event) => {
         switch (event.target.value) {
@@ -26,7 +28,11 @@ for (let option of menuOptions) {
             case 3: console.log("case3");
                 break;
             default: console.log("default")
-        }
+            }
 
-    })
+        })
+    }
 }
+
+flipCard(allCards)
+startMenu(allMenuChoices)
